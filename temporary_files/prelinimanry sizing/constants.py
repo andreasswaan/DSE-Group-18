140ffb84c9fd751ea4189340ba1abc9138cc3dda
pabloraichs <praichs@tudelft.nl>
2025-05-15 14:15:14 +0200

    test commit

--- temporary_files/prelinimanry sizing/constants.py
+++ temporary_files/prelinimanry sizing/constants.py
@@ -6,4 +6,4 @@ g = 9.80665  # Acceleration due to gravity in m/s^2
 battery_energy_density = (
     250 * 3600
 )  # J/kg, energy density of battery (placeholder value)
-L_over_D_cruise = 10.0  # Lift-to-drag ratio during cruise
+L_over_D_cruise = 10.0  # Lift-to-drag ratio during cruise test change
