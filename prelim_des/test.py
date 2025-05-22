7209863bbe542f56427999d13a6d751c2ea5f71c
Swaan Andreas <andreasswaan@gmail.com>
2025-05-22 12:05:56 +0200

    mission phases class completed

---
+++ prelim_des/test.py
@@ -0,0 +1,15 @@
+from mission import Mission
+
+
+mission = Mission(
+    "DR",
+    {"R": 120, "C": 120, "D": 30},
+    {"DR": 0, "RC": 2.5, "CC": 2, "CD": 0, "CR": 0},
+    {"R": 7, "C": 8, "D": 6},
+    {"R": 4, "C": 5, "D": 3},
+    {"DR": 1, "RC": 5, "CC": 5, "CD": 5},
+)
+print("Mission phases:", mission.phases_str)
+mission.phases[0].payload_mass = 100000
+print("Mission phase objects:", mission.phases[0])
+print("Mission phase objects:", mission.phases_dict[0])
