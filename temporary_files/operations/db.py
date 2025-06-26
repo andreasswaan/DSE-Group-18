ce2967105efcba14b94fb761dfd078196ddae5c2
Swaan Andreas <andreasswaan@gmail.com>
2025-06-26 16:42:36 +0200

    changed db file

--- temporary_files/operations/db.py
+++ temporary_files/operations/db.py
@@ -3,9 +3,7 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 # Use a service account.
-cred = credentials.Certificate(
-    "temporary_files\operations\dronebezorgd-7c265-firebase-adminsdk-fbsvc-3b56c8e485.json"
-)
+cred = credentials.Certificate("temporary_files\operations\dronebezorgd.json")
 
 app = firebase_admin.initialize_app(cred)
 
